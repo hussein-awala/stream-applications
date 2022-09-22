4a12747df12ef337b9bf033a163fe956cae0404f
Hussein Awala <hussein.awala@adevinta.com>
2022-09-22 02:11:16 +0200

    create a faker for two dev tables in order to use them to test stream join

---
+++ applications/streamers/src/main/java/avro/MessagesFakerMultipleTable.java
@@ -0,0 +1,10 @@
+package avro;
+
+public class MessagesFakerMultipleTable {
+    public static void main(String[] args) {
+        MessageFakerRunnable faker1 = new MessageFakerRunnable("table1", 10, 100, 100, "join/table1.avsc");
+        MessageFakerRunnable faker2 = new MessageFakerRunnable("table2", 60, 200, 50, "join/table2.avsc");
+        new Thread(faker1).start();
+        new Thread(faker2).start();
+    }
+}
